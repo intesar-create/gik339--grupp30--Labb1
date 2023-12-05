145727e14df533639b2861bf430f4d0dbccd7554
intesar <112574768+intesar-create@users.noreply.github.com>
2023-12-05 10:59:24 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+
+// Exempel på JavaScript-kod
+function myFunction() {
+    alert("Button clicked!");
+}
+
+// Hämta och manipulera HTML-elementet via JavaScript
+var myDiv = document.getElementById("myDiv");
